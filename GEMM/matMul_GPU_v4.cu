30eb1da96c29bc5c3f7af07e0e64e2c525979a3c
cuda_kernels <cuda@kernels.dev>
2026-05-31 21:06:26 +0000

    Sample push for v4 GPU matrix multiplication algorithm on GPU

---
+++ GEMM/matMul_GPU_v4.cu
@@ -0,0 +1,10 @@
+/**
+ * @file matMul_GPU_v4.cu
+ * @author Sudarshan Bandyopadhyay (sudb97@gmail.com)
+ * @brief This is the fourth version of the matrix multiplication kernel. This is the most optimized version of the kernel.
+ * @version 0.1
+ * @date 2026-06-01
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
